387ffd3da25489b95f20c274aa77d1460d5f53a2
Ayseky <ayse.gnn@outlook.com>
2025-03-09 01:50:29 +0300

    context sınıfı oluşturuldu ve migration işlemleri yapıldı.

---
+++ EasyCashIdentityProject.DataAcessLayer/Concrete/Context/EasyCashIdentityProjectDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAcessLayer.Concrete.Context
+{
+	public class EasyCashIdentityProjectDbContext : IdentityDbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=LAPTOP-EC1AVGPH;initial catalog=EasyCashDb;integrated Security=true;");
+		}
+
+		public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+		public DbSet<CustomerAccountProcess> CustomerAccountsProcess { get; set; }
+	}
+}
