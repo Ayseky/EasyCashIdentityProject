e0db12bd4cc6bc78deba6ddf99b7c9756b0ead77
Ayseky <ayse.gnn@outlook.com>
2025-03-15 20:45:56 +0300

    genericDal ve genericRepository oluşturuldu.

---
+++ EasyCashIdentityProject.DataAcessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using EasyCashIdentityProject.DataAcessLayer.Abstract;
+using EasyCashIdentityProject.DataAcessLayer.Concrete.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAcessLayer.Repositories
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		public void Delete(T entity)
+		{
+			using var context = new EasyCashIdentityProjectDbContext();
+			context.Set<T>().Remove(entity);
+			context.SaveChanges();
+		}
+
+		public List<T> GetAll()
+		{
+			using var context = new EasyCashIdentityProjectDbContext();
+			var datas= context.Set<T>().ToList();
+			return datas;
+		}
+
+		public T GetById(int id)
+		{
+			using var context = new EasyCashIdentityProjectDbContext();
+			var data = context.Set<T>().Find(id);
+			return data;
+		}
+
+		public void Insert(T entity)
+		{
+			using var context = new EasyCashIdentityProjectDbContext();
+            context.Set<T>().Add(entity);
+			context.SaveChanges();
+		}
+
+		public void Update(T entity)
+		{
+			using var context = new EasyCashIdentityProjectDbContext();
+			context.Set<T>().Update(entity);
+			context.SaveChanges();
+		}
+	}
+}
